87d00e4d8c50963ff7e52b17f07508030a76eab4
Reda Lemeden <reda@thoughtbot.com>
2014-12-22 22:53:31 +0000

    Add explicit size spec

--- spec/water/size_spec.rb
+++ spec/water/size_spec.rb
@@ -7,11 +7,21 @@ describe "-wtr-size()" do
 
   context "when passed 200px" do
     it "sets width to 200px" do
-      expect(".single-parameter").to have_rule("width: 200px")
+      expect(".implicit-height").to have_rule("width: 200px")
     end
 
     it "sets height to 200px" do
-      expect(".single-parameter").to have_rule("height: 200px")
+      expect(".implicit-height").to have_rule("height: 200px")
+    end
+  end
+
+  context "when passed 200px, 800px" do
+    it "sets width to 200px" do
+      expect(".explicit-height").to have_rule("width: 200px")
+    end
+
+    it "sets height to 800px" do
+      expect(".explicit-height").to have_rule("height: 800px")
     end
   end
 end
